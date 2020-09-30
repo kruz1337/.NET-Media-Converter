c3d7b857b8f04b7cf9811a42365a65149d38947c
TheRabbit2931 <61029407+TheRabbit2931@users.noreply.github.com>
2020-09-30 21:14:02 +0300

    fully_updated ~ 1.0

---
+++ DotNet_Converter/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DotNet_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Location:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Extract Directory:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 21);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Convert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".ac3",
+            ".aiff",
+            ".alaw",
+            ".asf",
+            ".ast",
+            ".au",
+            ".avi",
+            ".caf",
+            ".dts",
+            ".flac",
+            ".flv",
+            ".gif",
+            ".h261",
+            ".h263",
+            ".h265",
+            ".m4v",
+            ".matroska",
+            ".mjpeg",
+            ".mov",
+            ".mp4",
+            ".mpeg",
+            ".mulaw",
+            ".ogg",
+            ".oma",
+            ".rm",
+            ".swf",
+            ".webm",
+            ".wmv"});
+            this.comboBox1.Location = new System.Drawing.Point(101, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Convert format: ";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select Extract Directory.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = resources.GetString("openFileDialog1.Filter");
+            this.openFileDialog1.Title = "Select File Location.";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 99);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".NET Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DotNet_Converter/Form1.cs
@@ -0,0 +1,244 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNet_Converter
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox2.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == ".ac3")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.ac3", Format.ac3);
+                }
+                if (comboBox1.SelectedItem == ".aiff")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.aiff", Format.aiff);
+                }
+                if (comboBox1.SelectedItem == ".alaw")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.alaw", Format.alaw);
+                }
+                if (comboBox1.SelectedItem == ".asf")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.asf", Format.asf);
+                }
+                if (comboBox1.SelectedItem == ".ast")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.ast", Format.ast);
+                }
+                if (comboBox1.SelectedItem == ".au")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.au", Format.au);
+                }
+                if (comboBox1.SelectedItem == ".avi")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.avi", Format.avi);
+                }
+                if (comboBox1.SelectedItem == ".vaf")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.caf", Format.caf);
+                }
+                if (comboBox1.SelectedItem == ".dts")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.dts", Format.dts);
+                }
+                if (comboBox1.SelectedItem == ".eac3")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.eac3", Format.eac3);
+                }
+                if (comboBox1.SelectedItem == ".ffm")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.ffm", Format.ffm);
+                }
+                if (comboBox1.SelectedItem == ".flac")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.flac", Format.flac);
+                }
+                if (comboBox1.SelectedItem == ".flv")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.flv", Format.flv);
+                }
+                if (comboBox1.SelectedItem == ".gif")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.gif", Format.gif);
+                }
+                if (comboBox1.SelectedItem == ".h261")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.h261", Format.h261);
+                }
+                if (comboBox1.SelectedItem == ".h263")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.h263", Format.h263);
+                }
+                if (comboBox1.SelectedItem == ".h264")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.h264", Format.h264);
+                }
+                if (comboBox1.SelectedItem == ".h265")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.h265", Format.h265);
+                }
+                if (comboBox1.SelectedItem == ".m4v")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.m4v", Format.m4v);
+                }
+                if (comboBox1.SelectedItem == ".matroska")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.matroska", Format.matroska);
+                }
+                if (comboBox1.SelectedItem == ".mjpeg")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.mjpeg", Format.mjpeg);
+                }
+                if (comboBox1.SelectedItem == ".mov")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.mov", Format.mov);
+                }
+                if (comboBox1.SelectedItem == ".mp4")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.mp4", Format.mp4);
+                }
+                if (comboBox1.SelectedItem == ".mpeg")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.mpeg", Format.mpeg);
+                }
+                if (comboBox1.SelectedItem == ".mulaw")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.mulaw", Format.mulaw);
+                }
+                if (comboBox1.SelectedItem == ".ogg")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.ogg", Format.ogg);
+                }
+                if (comboBox1.SelectedItem == ".oma")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.oma", Format.oma);
+                }
+                if (comboBox1.SelectedItem == ".rm")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.rm", Format.rm);
+                }
+                if (comboBox1.SelectedItem == ".swf")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.swf", Format.swf);
+                }
+                if (comboBox1.SelectedItem == ".webm")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.webm", Format.webm);
+                }
+                if (comboBox1.SelectedItem == ".wmv")
+                {
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    ffMpeg.ConvertMedia(openFileDialog1.FileName, folderBrowserDialog1.SelectedPath + "\\" + System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.SafeFileName) + " - Converted by RConverter.wmv", Format.wmv);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString(), error.GetType().Name, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                button3.Enabled = false;
+            }
+            else
+            {
+                if (textBox2.Text == string.Empty)
+                {
+                    button3.Enabled = false;
+                }
+                else
+                {
+                    if (comboBox1.SelectedIndex == -1)
+                    {
+                        button3.Enabled = false;
+                    }
+                    else
+                    {
+                        button3.Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
